04b503727158675810576089b5edde6807b309db
wabbi-khan <kwahab789@gmail.com>
2023-04-05 08:47:27 -0700

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+// import AppContext from "./utils/context";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/category/:id",
+    element: <Category />,
+  },
+  {
+    path: "/product/:id",
+    element: <SingleProduct />,
+  },
+]);
+
 function App() {
-    return <div>App</div>;
+  return (
+    <main>
+      <Header />
+      <RouterProvider router={router}> </RouterProvider> <Newsletter />
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
