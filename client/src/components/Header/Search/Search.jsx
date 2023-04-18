0489e3e4f953ac746b717fafd14eee680c87d981
wabbi-khan <kwahab789@gmail.com>
2023-04-18 08:50:49 -0700

    search option done

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,7 +1,20 @@
 import "./Search.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
 import { MdClose } from "react-icons/md";
-import prod from "../../../assets/products/earbuds-prod-1.webp";
 const Search = ({ setShowSearch }) => {
+  const [query, setQuery] = useState([]);
+  const navigate = useNavigate();
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+  let { data } = useFetch(
+    `/api/products?populate=*&filters[title][$contains]=${query}`
+  );
+  if (!query.length) {
+    data = null;
+  }
   return (
     <div className='search-modal'>
       <div className='form-field'>
@@ -9,41 +22,42 @@ const Search = ({ setShowSearch }) => {
           autoFocus
           type='text'
           placeholder='Search for products'
-          // value={query}
-          // onChange={onChange}
+          value={query}
+          onChange={onChange}
         />
         <MdClose className='close-btn' onClick={() => setShowSearch(false)} />
       </div>
       <div className='search-result-content'>
-        {/* {!data?.data?.length && ( */}
-        <div className='start-msg'>
-          Start typing to see products you are looking for.
-        </div>
-        {/* )} */}
-        <div className='search-results'>
-          {/* {data?.data?.map((item) => ( */}
-          <div
-            className='search-result-item'
-            // key={item.id}
-            // onClick={() => {
-            //     navigate("/product/" + item.id);
-            //     setSearchModal(false);
-            // }}
-          >
-            <div className='image-container'>
-              <img src={prod} alt='search item img' />
-            </div>
-            <div className='prod-details'>
-              <span className='name'>
-                Product name {/* {item.attributes.title} */}
-              </span>
-              <span className='desc'>
-                Product des
-                {/* {item.attributes.description} */}
-              </span>
-            </div>
+        {!data?.data?.length && (
+          <div className='start-msg'>
+            Start typing to see products you are looking for.
           </div>
-          {/* ))} */}
+        )}
+        <div className='search-results'>
+          {data?.data?.map((item) => (
+            <div
+              key={item.id}
+              className='search-result-item'
+              onClick={() => {
+                navigate("/product/" + item.id);
+                setShowSearch(false);
+              }}
+            >
+              <div className='image-container'>
+                <img
+                  src={
+                    process.env.REACT_APP_DEV_URL +
+                    item?.attributes?.img?.data[0]?.attributes?.url
+                  }
+                  alt='search item img'
+                />
+              </div>
+              <div className='prod-details'>
+                <span className='name'>{item.attributes.title}</span>
+                <span className='desc'>{item.attributes.desc}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
