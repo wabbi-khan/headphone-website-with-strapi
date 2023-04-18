edb83b807c1b1c1cb367440882e8aaea78ca546f
wabbi-khan <kwahab789@gmail.com>
2023-04-18 09:32:23 -0700

    all done

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -4,9 +4,26 @@ import { BsCartX } from "react-icons/bs";
 import CartItem from "./CartItem/CartItem";
 import { useContext } from "react";
 import { Context } from "../../utils/context";
-
+import { loadStripe } from "@stripe/stripe-js";
+import { makePaymentRequest } from "../../utils/api";
 const Cart = ({ setShowCart }) => {
   const { cartItems, cartSubTotal } = useContext(Context);
+  const stripePromise = loadStripe(
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+  );
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest.post("/api/orders", {
+        products: cartItems,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className='cart-panel'>
       <div
@@ -41,10 +58,7 @@ const Cart = ({ setShowCart }) => {
                 <span className='text total'>$ {cartSubTotal}</span>
               </div>
               <div className='button'>
-                <button
-                  className='checkout-cta'
-                  // onClick={handlePayment}
-                >
+                <button className='checkout-cta' onClick={handlePayment}>
                   Checkout
                 </button>
               </div>
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 const params = {
-    headers: {
-        Authorization: "bearer " + process.env.REACT_APP_STRAPI_KEY,
-    },
+  headers: {
+    Authorization: "bearer " + process.env.REACT_APP_STRAPI_KEY,
+  },
 };
-export const fetchDataFromApi = async(url) => {
-    try {
-        const { data } = await axios.get(
-            process.env.REACT_APP_DEV_URL + url,
-            params
-        );
-        return data;
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+export const fetchDataFromApi = async (url) => {
+  try {
+    const { data } = await axios.get(
+      process.env.REACT_APP_DEV_URL + url,
+      params
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
+
+export const makePaymentRequest = axios.create({
+  baseURL: process.env.REACT_APP_DEV_URL,
+  headers: {
+    Authorization: "bearer " + process.env.REACT_APP_STRAPI_KEY,
+  },
+});
